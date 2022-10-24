511d8dbb12d5b8a1b0ade3213b8e252877ce4ecd
Abdullah Omar <fishilike.fish@gmail.com>
2022-10-25 03:22:58 +0600

    feat: #6 svg icons on bottom tab navbar

--- src/TabNavigator/TabNavigator.js
+++ src/TabNavigator/TabNavigator.js
@@ -11,6 +11,8 @@ import Calculator from '../Calculator/Calculator';
 import {AuthContext} from '../Auth/AuthProvider';
 import {Pressable} from 'native-base';
 
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+
 const TabNavigator = () => {
   const {signOut} = useContext(AuthContext);
 
@@ -24,10 +26,50 @@ const TabNavigator = () => {
           color: 'transparent',
         },
       }}>
-      <Tab.Screen name="Notification" component={Notification} />
-      <Tab.Screen name="Photo" component={Photo} />
-      <Tab.Screen name="Notes" component={Text} />
-      <Tab.Screen name="Calculator" component={Calculator} />
+      <Tab.Screen
+        name="Notification"
+        component={Notification}
+        options={() => {
+          return {
+            tabBarIcon: ({focused, color, size}) => (
+              <MaterialIcon name="notifications" size={size} color={color} />
+            ),
+          };
+        }}
+      />
+      <Tab.Screen
+        name="Photo"
+        component={Photo}
+        options={() => {
+          return {
+            tabBarIcon: ({focused, color, size}) => (
+              <MaterialIcon name="photo" size={size} color={color} />
+            ),
+          };
+        }}
+      />
+      <Tab.Screen
+        name="Note"
+        component={Text}
+        options={() => {
+          return {
+            tabBarIcon: ({focused, color, size}) => (
+              <MaterialIcon name="text-snippet" size={size} color={color} />
+            ),
+          };
+        }}
+      />
+      <Tab.Screen
+        name="Calculator"
+        component={Calculator}
+        options={() => {
+          return {
+            tabBarIcon: ({focused, color, size}) => (
+              <MaterialIcon name="calculate" size={size} color={color} />
+            ),
+          };
+        }}
+      />
       <Tab.Screen
         name="Sign Out"
         children={() => null}
@@ -35,6 +77,9 @@ const TabNavigator = () => {
           return {
             tabBarButton: props => (
               <Pressable {...props} onPress={signOut}></Pressable>
+            ),
+            tabBarIcon: ({size, color}) => (
+              <MaterialIcon name="logout" size={size} color={color} />
             ),
           };
         }}
